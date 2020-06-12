65cb7f8aa3251e45eb3fff8e941f4729b8a45307
Ray-Garraty <vl.besed@gmail.com>
2020-06-12 11:08:45 +0300

    completed the fourth step

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
+
 const welcome = () => {
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
-export { welcome };
+export default welcome;
